57ad67d7c8d7d020814b2c34ed34e6301058a586
k-minoda <fko2447028@stu.o-hara.ac.jp>
2025-04-15 14:56:24 +0900

    loginaction 変更

--- src/scoremanager/LoginAction.java
+++ src/scoremanager/LoginAction.java
@@ -1,5 +1,17 @@
 package scoremanager;
 
-public class LoginAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import tool.Action;
+
+public class LoginAction extends Action {
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// ユーザー名とパスワードを取得
+		String id = req.getParameter("id");
+		String password = req.getParameter("password");
+		// 認証チェック
+
+	}
 }
