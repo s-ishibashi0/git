95d9bc46eb9b501c8b0ef4257db17902ffdf4356
k-minoda <fko2447028@stu.o-hara.ac.jp>
2025-04-15 09:49:55 +0900

    tool action

--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -1,5 +1,19 @@
 package tool;
 
-public class FrontController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+	}
 }
