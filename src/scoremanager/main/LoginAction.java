a5643ba2c6c3688bc812ae50d2e0c7c3a8699ee8
k-minoda <fko2447028@stu.o-hara.ac.jp>
2025-04-16 14:52:17 +0900

    encoding henkou

--- src/scoremanager/main/LoginAction.java
+++ src/scoremanager/main/LoginAction.java
@@ -7,7 +7,7 @@ import tool.Action;
 
 public class LoginAction extends Action {
 	@Override
-	public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		// ユーザー名とパスワードを取得
 		String id = req.getParameter("id");
 		String password = req.getParameter("password");
--- src/tool/EncodingFilter.java
+++ src/tool/EncodingFilter.java
@@ -8,9 +8,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebFilter;
 
-@WebServlet(urlPatterns = { "/*" })
+@WebFilter(urlPatterns = { "/*" })
 public class EncodingFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
