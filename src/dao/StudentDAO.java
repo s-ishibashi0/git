acd93a3e1f888fa57f9b4d98d6179b49b19e1b61
s-ishibashi0 <fko2447058@stu.o-hara.ac.jp>
2025-04-16 10:46:05 +0900

    bean

---
+++ src/dao/StudentDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.sql.ResultSet;
+import bean.Student;
+
+public class StudentDAO extends DAO {
+	public Student get(String no) throws Exception{
+
+	}
+	prvate List<Student> postFilter(ResultSet rSet, School school) throw Exception{
+
+	}
+}
