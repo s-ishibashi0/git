2dac463a6fbf6858683fb10e46b473697ad2d85b
k-minoda <fko2447028@stu.o-hara.ac.jp>
2025-04-23 14:18:53 +0900

    subli comit

--- src/scoremanager/main/SubjectListAction.java
+++ src/scoremanager/main/SubjectListAction.java
@@ -5,6 +5,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import tool.Action;
 
-public abstract class SubjectListAction extends Action{
-	public abstract void execute(HttpServletRequest req, HttpServletResponse res) throws Exception;
-}
+public void SubjectListAction extends Action{
+	public abstract void execute(HttpServletRequest req, HttpServletResponse res) throws Exception
+	{try{
+		SubjectDAO dao = new SubjectDAO();
+		List<Subject> list = dao.getAllSubjects();
+		req.setAttribute("list",list);
+
+		req.getRequestDispatcher("Subject_list.jsp").forward(req,res);
+	}catch (Exception e) {
+		e.printStackTrace();
+	}
+	return "Subject_list.jsp";
+	}
+	}
